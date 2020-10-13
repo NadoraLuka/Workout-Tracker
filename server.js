7c3dac4e45955196cfcf3f1bf69bf9adde4b18ee
NadoraLuka <69468234+NadoraLuka@users.noreply.github.com>
2020-10-12 23:45:40 -0400

    commit for deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger("dev"));
